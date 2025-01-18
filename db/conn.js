e558faba56dfca1aa7ac61c751ced440d6d2bc41
Gustavo <gustavosoaresok@gmail.com>
2025-01-18 18:01:17 -0300

    banco

--- db/conn.js
+++ db/conn.js
@@ -1,14 +1,21 @@
-const {Sequelize} = require('sequelize')
+const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize('toughts2','root', '',{
-    host:'localhost',
-    dialect:'mysql',
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectModule: require('pg'),
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 })
 
-try{
-    sequelize.authenticate()
-    console.log('conectamos com sucesso')
-}catch(err){
-    console.log(`não foi possivel conectar:${err}`)
+try {
+  sequelize.authenticate()
+  console.log('Conectado ao PostgreSQL!')
+} catch (err) {
+  console.error('Não foi possível conectar:', err)
 }
+
 module.exports = sequelize
