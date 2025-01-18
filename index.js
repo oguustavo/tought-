5bebee2916079bd11e2a0ce0829981eb97e48704
Gustavo <gustavosoaresok@gmail.com>
2025-01-18 18:05:22 -0300

    a

--- index.js
+++ index.js
@@ -59,6 +59,9 @@ conn
 //.sync({force:true})
 .sync()
 .then(()=>{
-    app.listen(3000)
+    const PORT = process.env.PORT || 3000
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`)
+    })
 })
 .catch((err)=> console.log(err))
